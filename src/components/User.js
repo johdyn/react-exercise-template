2a5cc6e77853e743a3a374573a55e78624779e54
Johan Dynnesen <johandynnesen@gmail.com>
2021-04-29 19:51:26 +0200

    last push

--- src/components/User.js
+++ src/components/User.js
@@ -1,3 +1,12 @@
-function User() {}
-
-export default User;
+export default function User({ title, firstName, lastName, gender, picture }) {
+  return (
+    <div className={gender === "male" ? "maleUserBox" : "femaleUserBox"}>
+      <section className="user-class">
+        <h3>
+          {title} {firstName} {lastName}
+        </h3>
+        <img src={picture} />
+      </section>
+    </div>
+  );
+}
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,3 +1,44 @@
-function UserList() {}
+import { useEffect, useState } from "react";
+import User from "./User";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    console.log("first render");
+    const url =
+      "https://randomuser.me/api/?inc=email,gender,name,picture&results=10";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.results);
+        console.log(data);
+      });
+  }, []);
+
+  function renderUsers() {
+    console.log(users);
+    const userComponents = users.map((user) => (
+      <User
+        title={user.name.title}
+        firstName={user.name.first}
+        lastName={user.name.last}
+        gender={user.gender}
+        picture={user.picture.medium}
+      />
+    ));
+    return userComponents;
+  }
+
+  return (
+    <article className="UserList">
+      <p></p>
+      {renderUsers()}
+    </article>
+  );
+}
 
 export default UserList;
+
+// Husk clean up function
